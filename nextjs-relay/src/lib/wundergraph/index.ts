91fbcd4e0f93ddfd3cbb4cb7a7a4043ddd945724
DaniAkash <s.daniakash@gmail.com>
2023-04-12 12:47:05 +0530

    feat: setup nextjs example

---
+++ nextjs-relay/src/lib/wundergraph/index.ts
@@ -0,0 +1,11 @@
+import { createWunderGraphRelayApp } from "./wundergraph";
+import { createClient } from "../../../.wundergraph/generated/nextjs/client";
+
+const client = createClient();
+
+export const {
+  createClientEnvironment,
+  createServerEnvironment,
+  useLivePreloadedQuery,
+  withWunderGraphRelay,
+} = createWunderGraphRelayApp(client);
---
+++ nextjs-relay/src/pages/index.tsx
@@ -0,0 +1,150 @@
+import Image from "next/image";
+import { PreloadedQuery, graphql, usePreloadedQuery } from "react-relay";
+import { RelayProps } from "relay-nextjs";
+import { pagesDragonsQuery as PagesDragonsQueryType } from "../__relay__generated__/pagesDragonsQuery.graphql";
+import { Dragon } from "@/components/Dragon";
+import { withWunderGraphRelay } from "@/lib/wundergraph";
+
+const PagesDragonsQuery = graphql`
+  query pagesDragonsQuery {
+    spacex_dragons {
+      ...Dragons_display_details
+    }
+  }
+`;
+
+const DragonsList = ({
+  queryReference,
+}: {
+  queryReference: PreloadedQuery<
+    PagesDragonsQueryType,
+    Record<string, unknown>
+  >;
+}) => {
+  const data = usePreloadedQuery(PagesDragonsQuery, queryReference);
+
+  return (
+    <div>
+      <p>Dragons:</p>
+      {data.spacex_dragons?.map((dragon, dragonIndex) => {
+        if (dragon)
+          return <Dragon key={dragonIndex.toString()} dragon={dragon} />;
+        return null;
+      })}
+    </div>
+  );
+};
+
+function Home({ preloadedQuery }: RelayProps<{}, PagesDragonsQueryType>) {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
+          Get started by editing&nbsp;
+          <code className="font-mono font-bold">src/pages/index.tsx</code>
+        </p>
+      </div>
+
+      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] m-8"
+          src="/wundergraph.png"
+          alt="WunderGraph Logo"
+          width={180}
+          height={37}
+          priority
+        />
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert m-8"
+          src="/next.svg"
+          alt="Next.js Logo"
+          width={180}
+          height={37}
+          priority
+        />
+        <Image
+          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] m-8"
+          src="/relay.svg"
+          alt="Relay Logo"
+          width={180}
+          height={37}
+          priority
+        />
+      </div>
+
+      <DragonsList queryReference={preloadedQuery} />
+
+      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+        <a
+          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Docs{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Find in-depth information about Next.js features and API.
+          </p>
+        </a>
+
+        <a
+          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Learn{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Learn about Next.js in an interactive course with&nbsp;quizzes!
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Templates{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Discover and deploy boilerplate example Next.js&nbsp;projects.
+          </p>
+        </a>
+
+        <a
+          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
+          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <h2 className={`mb-3 text-2xl font-semibold`}>
+            Deploy{" "}
+            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+              -&gt;
+            </span>
+          </h2>
+          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+            Instantly deploy your Next.js site to a shareable URL with Vercel.
+          </p>
+        </a>
+      </div>
+    </main>
+  );
+}
+
+export default withWunderGraphRelay(Home, PagesDragonsQuery);
